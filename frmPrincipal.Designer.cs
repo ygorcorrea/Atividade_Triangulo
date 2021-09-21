c4aae578a45931cd8a2033c1f6e2364fad06a1bc
Ygor Correa <79712690+ygorcorrea@users.noreply.github.com>
2021-09-21 20:52:30 -0300

    Add files via upload

---
+++ frmPrincipal.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace Atividade_Luciano
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btExecutar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(47, 71);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(181, 20);
+            this.txtA.TabIndex = 0;
+            this.txtA.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(47, 123);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(181, 20);
+            this.txtB.TabIndex = 1;
+            this.txtB.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(47, 177);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(181, 20);
+            this.txtC.TabIndex = 2;
+            this.txtC.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Lado A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Lado B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Lado C";
+            // 
+            // btExecutar
+            // 
+            this.btExecutar.Location = new System.Drawing.Point(47, 223);
+            this.btExecutar.Name = "btExecutar";
+            this.btExecutar.Size = new System.Drawing.Size(181, 23);
+            this.btExecutar.TabIndex = 6;
+            this.btExecutar.Text = "Executar";
+            this.btExecutar.UseVisualStyleBackColor = true;
+            this.btExecutar.Click += new System.EventHandler(this.btExecutar_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 287);
+            this.Controls.Add(this.btExecutar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Name = "frmPrincipal";
+            this.Text = "Triângulos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btExecutar;
+    }
+}
+
---
+++ frmPrincipal.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividade_Luciano
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btExecutar_Click(object sender, EventArgs e)
+        {
+            int a, b, c;
+            a = Convert.ToInt32(txtA.Text);
+            b = Convert.ToInt32(txtB.Text);
+            c = Convert.ToInt32(txtC.Text);
+
+            if ((a < b + c) && (b < a + c) && (c < a + b))
+            {
+               
+                Console.Write("Formam triângulo!");
+
+                if ((a == b) && (b == c))
+                {
+
+                    MessageBox.Show("Triângulo Equilátero");
+
+                }
+                else if ((a == b) || (b == c) || (a == c))
+                {
+
+                    MessageBox.Show("Triângulo Isóceles");
+                }
+                else
+                {
+
+                    MessageBox.Show("Triângulo Escaleno");
+                }
+
+            }
+            else
+            {
+
+                MessageBox.Show("Os valores não formam um triangulo");
+            }
+        }
+    }
+}
